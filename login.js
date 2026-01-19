74b55d4434c253e3053eb66540f72a08e4b2bf9f
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-01-19 12:40:28 +0300

    commented js

--- login.js
+++ login.js
@@ -1,21 +1,21 @@
-const form = document.querySelector('.form');
-const bg = document.querySelector('.fakebg');
+// const form = document.querySelector('.form');
+// const bg = document.querySelector('.fakebg');
 
-form.addEventListener('mouseenter', () => {
-    bg.style.background = "linear-gradient(to bottom, #000000, #002860db, #ff0000, #276fe3, #000000)";
-    bg.style.transition = "5s";
+// form.addEventListener('mouseenter', () => {
+//     bg.style.background = "linear-gradient(to bottom, #000000, #002860db, #ff0000, #276fe3, #000000)";
+//     bg.style.transition = "5s";
 
-}
-);
+// }
+// );
 
-form.addEventListener('mouseleave', () => {
-    bg.style.transition = "5s";
-    bg.style.background = "linear-gradient(to bottom, #000000,  #ff0000, #276fe3,#002860db, #000000)";
-}
-);
-from.addEventListener('mouseenter', () => {
-    bg.style.filter = "blur(0px)"
-})
-from.addEventListener('mouseleave', () => {
-    bg.style.filter = "blur(0px)"
-})
+// form.addEventListener('mouseleave', () => {
+//     bg.style.transition = "5s";
+//     bg.style.background = "linear-gradient(to bottom, #000000,  #ff0000, #276fe3,#002860db, #000000)";
+// }
+// );
+// from.addEventListener('mouseenter', () => {
+//     bg.style.filter = "blur(0px)"
+// })
+// from.addEventListener('mouseleave', () => {
+//     bg.style.filter = "blur(0px)"
+// })
